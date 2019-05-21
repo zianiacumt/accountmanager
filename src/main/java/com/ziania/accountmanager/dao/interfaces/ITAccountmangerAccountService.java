bd4ef2807f24b97e2c348fc1060498f1ff97dfdd
ziania <cumt_czn0805@126.com>
2019-05-21 21:06:59 +0800

    20190521

--- src/main/java/com/ziania/accountmanager/dao/interfaces/ITAccountmangerAccountService.java
+++ src/main/java/com/ziania/accountmanager/dao/interfaces/ITAccountmangerAccountService.java
@@ -1,7 +1,7 @@
 package com.ziania.accountmanager.dao.interfaces;
 
 import com.ziania.accountmanager.exception.CommonException;
-import java.util.List;
+
 import java.util.Map;
 
 public interface ITAccountmangerAccountService {
